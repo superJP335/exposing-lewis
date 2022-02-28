549e2ac634fd63c5010307a22418d0d1eed88894
superJP335 <76703807+superJP335@users.noreply.github.com>
2022-02-28 11:45:05 -0500

    initial release

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Exposing Lewis/YourWRLD/SilentCloud/Bailey</title>
+    <meta name="keywords" content="lewisfked, lewis pedo, retrimo, brickistic, voixed, bricktoria, bloxpatched, yourwrld, silentcloud">
+    <meta name="description" content="An entire website dedicated to expose Lewis/YourWRLD/SilentCloud. His disturbing behavior cannot continue on this way. This website will be updated frequently to include his new alternative accounts.">
+    <meta name="author" content="The Sandbox Community">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="site.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <ul class="navbar">
+      <li><a href="#origin">Origin</a></li>
+      <li><a href="#breakdown">The Breakdown</a></li>
+      <li><a href="#alts">Alt accounts</a></li>
+      <li><a href="#conclusion">Conclusion</a></li>
+      <li><a href="#contribute">Contribute</a></li>
+    </ul>
+    <div class="notice">
+      i recommend that u have 2 eyes, a brain thats functional, and common sense before u read. if you dont, heres a vid describing you <a href="https://youtu.be/-iOzHoxsor4" target="_blank">here</a>
+      </div>
+    <div class="subhead">
+      <h1 id="expose" class="center">Exposing Lewis</h1>
+    </div>
+    <div class="subhead">
+      <p class="center">This website is dedicated to expose Lewis/YourWRLD/SilentCloud. His disturbing behavior cannot continue on this way, as he has impacted many individuals in a negative way. This website will be updated frequently to include his new alternative accounts and horrible actions he has committed.</p>
+    </div>
+    <div class="card card-center">
+      <h2 id="origin">Background/Origin</h2>
+      <p>Lewis was a member in the Sandbox Community for a while, I met him back at Retrimo.com, however, he was there much longer before that as he had already started a few projects such as BloxWorld. He was a Discord Moderator for Retrimo and later took on other jobs such as being a staff member in Novacubes.net, started a few projects like Vernium.net, Retrimo.net, and many others. He was also active on the ROBLOX Community, owning a few role-playing games which acted similar to games like Da Hood or The Streets. Believe it or not, I used to be a friend of Lewis, I worked for several of his projects and he would ask me to help him with programming and making items.</p>
+    </div>
+    <div class="card card-center">
+      <h2 id="breakdown">The Breakdown</h2>
+      <p>Before mentioning the act that started all of this chaos, I would like to mention his status in the community before he was exposed as a pedophile: </p>
+      <ul>
+        <li>Stealing source codes for his websites</li>
+        <li>Lack of credibility</li>
+        <li>Stealing artwork for his websites</li>
+        <li>Other stuff I can't remember</li>
+      </ul>
+      <p>Yeah, it wasn't in the best shape. Now onto the part everyone waited for...</p>
+      <b>Him exposing himself as a pedophile!</b>
+      <p>It started when he added me on one of his alt accounts and starting grooming me basically, below are the screenshots of his messages:</p>
+      <div class="images">
+        <img src="/images/fhjmvv.PNG">
+        <img src="/images/fhjm.PNG">
+        <img src="/images/zdv.PNG">
+      </div>
+      <p>Yikes, am I right? Oh and you might think it was just someone who impersonated him, so here is a screenshot of him admitting to be IAmACutie:</p>
+      <img src="/images/dfsv.PNG">
+      <p>And now, for the one message which costed him his entire reputation to revert to zero:</p>
+      <img src="/images/dvf.PNG">
+      <p>That is his actual dick, I did not make this up. This was the final nail in the coffin for him, after he sent that, I took all the necessary screenshots and made a huge announcement on a site I used to own, Brickorzo. After that, it sparked outrage, many raided his current website (Brickistic) and harassed him. It caused him to be banned from his own website (really) by a staff called LexLuthor. He then proceeded to make a banner on the website stating this:</p>
+      <img style="width:800px;" src="/images/NGH.PNG">
+      <p>Wow, amazing response. Telling everyone that he will kill himself instead of assuming and getting mental help. That, my friends is called an Icseon Move. He then posted a video link of his said "suicidal" which turned out to be a rick-roll. If you get exposed as a pedophile, and ur reaction is to fake suicide to earn sympathy and post a rick-roll, you are fucking human waste at this point. Not only he doesn't give a shit, but he turns it into a meme. Suicide is not a joking manner, and never will be.</p>
+      <p>In the same day, YourWRLD AKA Lewis DM's me, which I immediatly reply with the screenshots, but he starts defending himself, saying he is 13 and that he has Lewis's social medias & ID (His real life ID, already a bad start for lying) and other things. Surprisignly enough, I fucking believed he wasn't Lewis somehow, I then worked for YW's websites and considered him a "friend" (I was dumb at the time) It took so long for me to realize he was Lewis, actually, the person who made me realize it was him was Studzy. He said YourWRLD joined Retrimo the day Lewis got exposed and immediattly defended Lewis, saying he was 13, and that his account was brand new, a few hours old to be specific.</p>
+      <p>He then sends me a photo of Lewis' half assed apology he wrote in 5 minutes to try calm people down:</p>
+      <img id="apo" style="width:800px;" src="/images/halfassedapology.png">
+      <p>It clearly shows he didn't care the slightest about the situation, and saw it as an other drama. Going as far as to accuse other people that they lied and just tried to get the buzz. I can assure you these people had real things to go against Lewis, but since I was the victim, he brushes them off... also, this apology will be VERY important later on in the documents, as it solidifies future points that I will be sharing.</p>
+    </div>
+    <div class="card card-center">
+      <h2 id="alts">Alts accounts</h2>
+      <p>As all pedophiles that don't know how to get do, he makes alt accounts ALL THE TIME. Everytime we expose his alts as Lewis, he abandons them and makes a new one. These alts are very easy to identify, as they will use the same text layout & format when making announcements on the alts' discords for sites (Refer to the half assed apology for the comparison)</p>
+      <p>Here is a list of all of Lewis' confirmed alts:</p>
+      <ul>
+        <li>YourWRLD</li>
+        <li>SilentCloud (Renamed to Bailey)</li>
+        <li>Gh_ost (Unknown alt to the community)</li>
+      </ul>
+      <p>We have already discussed about YourWRLD, so, we will go straight to Bailey. What you are about to witness is the truth but only the truth. Because I want YOU, the person reading this to know the real person who is behind the computer screen.</p>
+      <b>SilentCloud:</b>
+      <p>I technically trapped him day 1 on the Mopaiv discord server, the messages are not there anymore as he left Mopaiv (or was banned). But I noticed he was talking about domains & YourWRLD, so I was getting suspicious, I asked him a very simple question, which was if he knew Avasquare.com, he said no, but clearly he just contradicted himself: </p>
+      <img style="width:500px;" src="/images/Capture_2022-02-01-16-10-09.png">
+      <img style="width:500px;" src="/images/Capture_2022-02-01-16-10-21.png">
+      <p>He dismissed that fact as him "forgetting" about Avasquare, which is a terrible excuse, and he then continued on his day at Mopaiv, despite what just happened.</p>
+      <p>Remember when I said his alts talk the same and he announces things in the same format? Well.. (<a href="#apo">layout in question</a>)</p>
+      <img src="/images/dfvxz.PNG">
+      <p>If you have atleast 2 eyes and that they are fully functional, it's a dead giveaway. The emoji infront of the title, bolded title, bolding the important aspects, and pinging at the end of the announcement.</p>
+      <p>Now, you might say that I am going WAY too far, that im picking anything to expose him, but let me remind you with who are we dealing with. It is not some random script kiddie who called someone a racial slur, it is a PEDOPHILE who is constantly hiding into the SBC (which is full of kids!)</p>
+      <p>An other thing to mention is how he logged on to his old Lewis account and made it join his own fucking server to talk to himself or to try making others think Bailey & Lewis are 2 different people. This immediatly lifts up a red flag, as if he was not Bailey/SilentCloud, then how could he have known that BCP existed? How could he have had guessed the invite code without anyone telling him? The answer is simple, Lewis is Bailey/SilentCloud/YourWRLD. And after he finished and a member asked him about it, he BLOCKS the member. When you do something like that, it just makes the evidence factual, as if you are trying to hide something:</p>
+      <img src="https://cdn.discordapp.com/attachments/939889507518517360/947280870367191040/unknown.png">
+      <img src="https://cdn.discordapp.com/attachments/794577234332483644/947281300627288104/unknown.png">
+      <p>Now, after all you just witnessed, after all the evidence and proof, you think we are in the wrong because of 1 small detail I will be disclosing, well ur pretty retarded, let me show you, the 1 excuse that was keeping him alive up to this point, the excuse that will now be worthless, it is.....</p>
+      <b>I am 13!</b>
+      <p>But now, take a look at this! Oh no! He is fucking contradicting himself yet again!</p>
+      <img src="https://cdn.discordapp.com/attachments/947283257727275108/947585040806727700/bfvb.PNG">
+      <img src="https://cdn.discordapp.com/attachments/947283257727275108/947585041003843594/xc.PNG">
+      <p>These messages were said in the Bopimo.com discord server, a server his Lewis account is still in, you can clearly see he is implying that he is <b>17</b> and NOT <b>13</b></p>
+      <p>Incase ur too lazy to do the math, heres the solution: 11 (years he spent in the uk) + 4 (years he spent in spain) + 2 (years he spent in russia) = 17. So easy I didnt even need to use a calculator!</p>
+      <p>Now, onto the conclusion.</p>
+    </div>
+    <div class="card card-center">
+      <h2 id="conclusion">Conclusion</h2>
+      <p>So, my friends, we can now finally conclude that Lewis/YourWRLD/SilentCloud/Bailey is a pedophile and that YourWRLD/SilentCloud/Bailey is Lewis!</p>
+      <p>The fact that he still has a platform and that he gets support and people who defend him and harass me because he manipulates others to believe the mountain of evidence is fake just blows my mind. The fact he is trying so hard to hide the truth and continues being on the SBC disgusts me. But whatever this pedophile does to stop the truth, I will counter, because the best thing in life is the truth.</p>
+      <p>There are even retards like Hondo who think that I (superJP335) is Lewis. I think he forgot what was common sense, but i'll fill him in. If I was Lewis, this entire website would be like shooting myself in the foot, as it could destroy my entire credibility & trustworthiness. I don't understand why you believe this entire site is fake, maybe because you were brainwashed? Idk and idc, hondos a fucking failure anyways.</p>
+      <p>Like I said, this will be frequently updated, to include new evidence, his new alt accounts, and his new practises. The website will also not be taken down, unless something extremely major happens.</p>
+    </div>
+    <div class="card card-center">
+      <h2 id="contribute">Contribute</h2>
+      <p>Do you want to share your experiences with Lewis on the website? Well, we have opened an email specifically for those: <a href="mailto:fucklewis@unitorium.endl.site">fucklewis@unitorium.endl.site</a>. E-Mail us your story & experiences with him and we shall add it to this website for others to see and for your voice to be heard. <b>Your stories & experiences with him must be real, and it must include screenshots of them. If we find out that it is fake or just biased, we will not add it to the website.</b></p>
+    </div><br><br>
+    <p style="color:grey;font-size:12px;" class="center">Brought to you by: superJP335, Queezy, Bryn, Mark</p>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+      setInterval(function(){
+        var items = ['Exposing Lewis', 'Exposing YourWRLD', 'Exposing SilentCloud', 'Exposing Bailey'];
+        var item = items[Math.floor(Math.random() * items.length)];
+        $("#expose").text(item);
+      }, 1000)
+    </script>
+  </body>
+</html>
